d9169cbcc9c22ea4fa4dc588b511d463fd964f3b
abhijnanacharya <abhijnanacharya11@gmail.com>
2023-03-19 21:53:59 +0530

    fix

--- src/config/typeorm.module.ts
+++ src/config/typeorm.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'sqlite',
       database: 'db.sqlite',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: false,
       migrations: [__dirname + '/../db/migrations'],
     }),
   ],
