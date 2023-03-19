e7c4ebbac91d94573b4cbbf93ce563ff4b6cfd5c
abhijnanacharya <abhijnanacharya11@gmail.com>
2023-03-19 22:43:05 +0530

    fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,40 +5,41 @@ import { User } from './entity/user.entity';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-  ) {}
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) {}
 
-  async findAll(): Promise<User[]> {
-    return this.userRepository.find();
-  }
+    async findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
 
-  async findOne(id: number): Promise<User> {
-    return this.userRepository.findOne({
-      where: { id: id },
-    });
-  }
+    async findOne(id: number): Promise<User> {
+        return this.userRepository.findOne({
+            where: { id: id },
+        });
+    }
 
-  async create(user: User): Promise<User> {
-    const newUser = new User();
-    newUser.firstName = user.firstName;
-    newUser.lastName = user.lastName;
-    newUser.email = user.email;
-    newUser.password = user.password;
-    newUser.createdAt = parseInt(new Date().toISOString());
-    return this.userRepository.save(newUser);
-  }
+    async create(user: User): Promise<User> {
+        console.log('create new user');
+        const newUser = new User();
+        newUser.firstName = user.firstName;
+        newUser.lastName = user.lastName;
+        newUser.email = user.email;
+        newUser.password = user.password;
+        newUser.createdAt = parseInt(new Date().toISOString());
+        return this.userRepository.save(newUser);
+    }
 
-  async update(id: number, user: User): Promise<User> {
-    await this.userRepository.update(id, user);
-    return this.userRepository.findOne({
-      where: { id: id },
-    });
-  }
+    async update(id: number, user: User): Promise<User> {
+        await this.userRepository.update(id, user);
+        return this.userRepository.findOne({
+            where: { id: id },
+        });
+    }
 
-  async delete(id: number): Promise<void> {
-    console.log('Helloooooo!!!!');
-    await this.userRepository.delete(id);
-  }
+    async delete(id: number): Promise<void> {
+        console.log('Helloooooo!!!!');
+        await this.userRepository.delete(id);
+    }
 }
